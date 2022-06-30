d1b5309612f948feef9da3121073d01448599bb3
RichOmaar <39716286+RichOmaar@users.noreply.github.com>
2022-06-30 16:52:12 -0500

    Cambios URL

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 const config = {
     // urlApi: 'http://localhost:8888/GitHub/rinoexperts-api/',
-    urlApi: "http://tresmosqueteros.click/",
+    // urlApi: "http://tresmosqueteros.click/",
     urlApi: "http://tresmosqueteros.click.preview.services/",
-    urlWeb: "http://localhost:3000/"
-    // urlWeb: "https://cursodontic-web3.vercel.app/"
+    // urlWeb: "http://localhost:3000/"
+    urlWeb: "https://cursodontic-web3.vercel.app/"
 }
 
 export default config;
