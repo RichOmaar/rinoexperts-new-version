b5fe1f1089c6f14f627653acb90c224927b854f9
RichOmaar <39716286+RichOmaar@users.noreply.github.com>
2022-07-26 18:43:11 -0500

    Cambio de rutas

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 const config = {
-    urlApi: 'http://localhost:8888/GitHub/rinoexperts-api/',
-    // urlApi: "https://rinoexperts.kinesiuz.com/",
-    urlWeb: "http://localhost:3000/"
-    // urlWeb: "https://cursodontic-web3.vercel.app/"
+    // urlApi: 'http://localhost:8888/GitHub/rinoexperts-api/',
+    urlApi: "https://rinoexperts.kinesiuz.com/",
+    // urlWeb: "http://localhost:3000/"
+    urlWeb: "https://cursodontic-web3.vercel.app/"
 }
 
 export default config;
